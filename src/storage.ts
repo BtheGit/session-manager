8c55f3417dd54b129be8ff925789e6095679ee54
BtheGit <brendanwebdev@gmail.com>
2021-05-14 20:18:17 -0400

    Add secure value to cookie defaults

--- src/storage.ts
+++ src/storage.ts
@@ -2,6 +2,8 @@ import Cookie from "js-cookie";
 import { Session } from "./types";
 import { SESSION_COOKIE_ID } from "./constants";
 
+Cookie.defaults = { secure: true };
+
 export function readSession(): Session {
   return Cookie.getJSON(SESSION_COOKIE_ID);
 }
